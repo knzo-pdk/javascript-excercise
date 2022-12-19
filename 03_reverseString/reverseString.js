76470ef98694ebf820ad8eaa18e68c3e7fc66f58
Kenzo Pham <khoiduc1997@gmail.com>
2022-12-18 22:30:22 -0500

    done 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let i = string.length;
+    let returningStr = '';
+    while(i >= 0) {
+        let ch = string.charAt(i);
+        returningStr += ch;
+        i--;
+    }
+    return returningStr;
 
 };
 
