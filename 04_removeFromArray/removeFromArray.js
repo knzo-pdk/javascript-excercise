eceeda7820f94d6ba090b8106581a6f8731c9cd0
Kenzo Pham <khoiduc1997@gmail.com>
2022-12-19 01:48:40 -0500

    03 in progress

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, number) {
+    let i = 0;
+    let length = array.length-1;
+    let returnArr = [length];
+    while (i < length) {
+        if (array[i] === number) {
+            returnArr[i] = array[i+1];
+        } else {
+            returnArr[i] = array[i];
+        }
+        i++;
+    }
+    return returnArr;
 };
 
 // Do not edit below this line
